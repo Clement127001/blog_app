f29422a914a0dfda1a7eac2f5560f0c617a5a131
Clement_127001 <babiyonclement18@gmail.com>
2025-05-14 13:08:31 +0530

    complete blog controllers

--- server/controller/blog.js
+++ server/controller/blog.js
@@ -4,13 +4,44 @@ const User = require("../models/User");
 const { BadRequestError } = require("../errors");
 
 const getAllBlogs = async (req, res) => {
-  res.send("get all blogs");
+  const { author, category, pageNumber, pageSize } = req.query;
+
+  const convertedPageNumber = +pageNumber;
+  const convertedPageSize = +pageSize;
+
+  const skip = (convertedPageNumber - 1) * convertedPageSize;
+
+  const blogQuery = category ? { category } : {};
+
+  const blogs = await Blog.find(blogQuery)
+    .populate({
+      path: "author",
+      match: { name: { $regex: author, $options: "i" } },
+      select: "_id name",
+    })
+    .skip(skip)
+    .limit(convertedPageSize);
+
+  const filteredBlogs = blogs.filter((blog) => blog.author);
+
+  res.status(StatusCodes.OK).json({ blogs: filteredBlogs });
+};
+
+const checkBlogAvailability = async (blogId) => {
+  const blog = await Blog.findById(blogId);
+
+  if (!blog) {
+    throw new BadRequestError("Blog not found");
+  }
 };
 
 const getSingleBlog = async (req, res) => {
   const blogId = req.params.blogId;
 
-  const blog = await Blog.findOne({ _id: blogId }).populate("author");
+  const blog = await Blog.findOne({ _id: blogId }).populate(
+    "author",
+    "_id name"
+  );
 
   if (!blog) {
     return new BadRequestError("Blog not found");
@@ -34,6 +65,8 @@ const createBlog = async (req, res) => {
 const updateBlog = async (req, res) => {
   const blogId = req.params.blogId;
 
+  await checkBlogAvailability(blogId);
+
   await Blog.findOneAndUpdate({ _id: blogId }, { ...req.body });
 
   res.status(StatusCodes.OK).json({ msg: "Blog updated successfully" });
@@ -41,6 +74,8 @@ const updateBlog = async (req, res) => {
 
 const deleteBlog = async (req, res) => {
   const blogId = req.params.blogId;
+
+  await checkBlogAvailability(blogId);
 
   await Blog.findOneAndDelete({ _id: blogId });
 
